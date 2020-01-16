c557fac935dcdf6643a413b8332a4b234e031299
Evgeny Serikov <evgenyserikov@Mac-mini-Evgeny-2.local>
2020-01-16 16:37:50 +0300

    Create project

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, "12.0"
+use_frameworks!
+
+def app_pods
+  pod "ObjectMapper", "~> 3.4.2"
+  pod "Alamofire", "~> 4.8.1"
+end
+
+target "TestTask" do
+  app_pods
+end
---
+++ TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Evgeny Serikov on 16.01.2020.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
